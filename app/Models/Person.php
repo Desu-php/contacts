4b2c67969be62089175877888d990d525f2bb6c9
BAKHA <Teaddyson@gmail.com>
2021-06-17 23:16:46 +0500

    added foreing key

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -46,7 +46,7 @@ class Person extends Model
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class, 'person_tags', 'person_id', 'tag_id');
+        return $this->belongsToMany(Tag::class, 'person_tags', 'tag_id', 'person_id');
     }
 
     public function cities()
